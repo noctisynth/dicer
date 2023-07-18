816612a10ea1f0b0c97586ace5dac75004cef520
不信者 <fu050409@163.com>
2023-07-18 09:37:47 +0800

    更新scp检定结论.

--- dicer.py
+++ dicer.py
@@ -125,16 +125,18 @@ def expr(d, anum):
 def scp_doc(result, difficulty, agent=None, great=False):
     if not agent:
         agent = "该特工"
-    if difficulty > 25:
-        return f"检定结果: 致命失败.\n检定结论: {agent} 在试图挑战数学、挑战科学、挑战真理, 尝试达成一个不可能事件, {agent} 毫无疑问获得了 致命失败."
     r = f"事件难度: {difficulty}\n"
+    if difficulty > 25:
+        r += f"检定数据: {random.randint(1, 25)}"
+        r += f"检定结果: 致命失败.\n检定结论: {agent} 在试图挑战数学、挑战科学、挑战真理, 尝试达成一个不可能事件, {agent} 毫无疑问获得了 致命失败."
+        return r
     r += f"检定数据: {result}\n"
     if great:
         r += "检定结果: 关键成功.\n"
         if result <= difficulty:
-            r += "检定结论: 有时候, 一次普通的成功或许会大幅度的牵扯到整个未来."
+            r += "检定结论: 有时候, 一次普通的成功或许会大幅度的牵扯到整个未来, 但这并不是一件太过于值得高兴的事情, 因为它不见得是一个好的开始."
         else:
-            r += "检定结论: 被 Administrator 所眷顾的人, 毫无疑问这是一次完美的成功."
+            r += "检定结论: 被 Administrator 所眷顾的人, 毫无疑问这是一次完美的成功, 但是你或许会面对更加绝望的未来."
     elif result > difficulty:
         r += "检定结果: 成功.\n"
         r += "检定结论: 命运常常给予人们无声的嗤笑, 一次成功当然是好事, 但也要警惕这是否是步入深渊的开始."
@@ -143,9 +145,15 @@ def scp_doc(result, difficulty, agent=None, great=False):
         r += "检定结论: 努力或许的确有用处, 但是努力只是提高运气的一种手段. 在低劣的运气面前, 任何努力都是没有用的."
     elif result < difficulty:
         r += "检定结果: 失败.\n"
-        r += "检定结论: 小心, 错误的决定或许会让你步入深渊."
+        r += "检定结论: 人类从来都生活在饱含恐惧与绝望的危险之中, 失败是一件稀松平常的事情, 小心, 错误的决定或许会让你步入深渊."
     else:
-        r += "请重新检定."
+        result = random.randint(0, 1)
+        if result:
+            r += "检定结果: 成功.\n"
+            r += "检定结论: 小心, 你的成功完全是一次偶然, 不要试图去将这样的偶然当做希望."
+        else:
+            r += "检定结果: 失败.\n"
+            r += "检定结论: 成功与失败宛如山顶与深渊, 无论是哪一种都是可能的, 相反, 在这个世界落入深渊是一件更加合理的事情."
     return r
 
 if __name__ == "__main__":

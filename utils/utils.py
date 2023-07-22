d902289aa3ecd58b5568062bf551725d2d3203af
不信者 <fu050409@163.com>
2023-07-22 11:39:39 +0800

    修复"ss"指令不被识别的问题

--- utils/utils.py
+++ utils/utils.py
@@ -5,3 +5,6 @@ current_dir = Path(__file__).resolve().parent
 _coc_cachepath = current_dir.parent / "data" / "coc_cards.json"
 _scp_cachepath = current_dir.parent / "data" / "scp_cards.json"
 logger = logging.get_logger()
+
+def get_group_id(message):
+    return message.channel_id

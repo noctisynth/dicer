f06ae7933644e2bfe9819c94769648e68ef051ca
不信者 <fu050409@163.com>
2023-07-29 19:06:35 +0800

    版本3.0.1 修复了init函数

--- dicergirl/utils/utils.py
+++ dicergirl/utils/utils.py
@@ -25,19 +25,25 @@ import os
 import uuid
 import re
 import inspect
+import yaml
 
 current_dir = Path(__file__).resolve().parent
-_coc_cachepath = current_dir.parent / "data" / "coc_cards.json"
-_scp_cachepath = current_dir.parent / "data" / "scp_cards.json"
-_super_user = current_dir.parent / "data" / "super_user.json"
+dicer_girl_dir = Path.home() / ".dicergirl"
+data_dir = dicer_girl_dir / "data"
+_coc_cachepath = data_dir / "coc_cards.json"
+_scp_cachepath = data_dir / "scp_cards.json"
+_super_user = data_dir / "super_user.json"
 _log = logger
 su_uuid = (str(uuid.uuid1()) + str(uuid.uuid4())).replace("-", "")
-version = "3.0.0 稳定版"
+version = "3.0.1"
 
 def init():
-    if not current_dir / "data":
-        _log.info("[cocdicer] 数据文件夹未建立, 建立它.")
-        os.makedirs("data")
+    if not dicer_girl_dir.exists():
+        _log.info("Dicer Girl 数据文件夹未建立, 建立它.")
+        dicer_girl_dir.mkdir()
+    if not data_dir.exists():
+        _log.info("Dicer Girl 数据文件夹未建立, 建立它.")
+        data_dir.mkdir()
     if not os.path.exists(_coc_cachepath):
         _log.info("[cocdicer] COC存储文件未建立, 建立它.")
         with open(_coc_cachepath, "w", encoding="utf-8") as f:

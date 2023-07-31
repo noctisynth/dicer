b85e906c1b7b254d0c273eafd976b6f9f08718e5
不信者 <fu050409@163.com>
2023-07-31 18:23:14 +0800

    版本3.0.10

--- dicergirl/utils/utils.py
+++ dicergirl/utils/utils.py
@@ -42,7 +42,7 @@ elif package == "qqguild":
         class Message:
             pass
 
-version = "3.0.9"
+version = "3.0.10"
 current_dir = Path(__file__).resolve().parent
 dicer_girl_dir = Path.home() / ".dicergirl"
 data_dir = dicer_girl_dir / "data"

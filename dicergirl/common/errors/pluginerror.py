f6f8ed4dd7d7352391ea8016af164697c04b87b1
苏向夜 <fu050409@163.com>
2023-09-25 10:52:16 +0800

    :art: 设置errors注释

--- dicergirl/common/errors/pluginerror.py
+++ dicergirl/common/errors/pluginerror.py
@@ -1,11 +1,11 @@
 class PluginNotFoundError(FileNotFoundError):
-    ...
+    """ 插件不存在 """
 
 class PluginExistsError(FileExistsError):
-    ...
+    """ 插件已安装 """
 
 class PluginInstallFailedError(RuntimeError):
-    ...
+    """ 插件安装失败 """
 
 class PluginUninstallFailedError(RuntimeError):
-    ...
+    """ 插件卸载失败 """

36d4ad7c7aa288ec296198107f742cb2a3027ba0
苏向夜 <fu050409@163.com>
2024-03-04 14:46:57 +0800

    chore(dicergirl): move old version of dicergirl

--- pack.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del dist /F /Q
-C://Users/fu050/Desktop/Projects/DicerGirl/.venv/Scripts/python setup.py sdist bdist_wheel
-twine upload dist/*
-del dist /F /Q
-pause
--- pack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python setup.py sdist bdist_wheel
-twine upload dist/*
-rm dist/*

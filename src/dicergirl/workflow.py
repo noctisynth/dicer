b914d6da61d6f0db44a1a43113bad1339658eaa7
苏向夜 <fu050409@163.com>
2024-03-11 20:30:51 +0800

    feat(workflow): add workflow

--- src/dicergirl/workflow.py
+++ src/dicergirl/workflow.py
@@ -1,9 +1,9 @@
 from concurrent.futures import ThreadPoolExecutor
 from typing import Callable
-from .utils import hmr
+from .utils import hmr, file_upload
 
 pool = ThreadPoolExecutor(20)
-workflows = {"echo.hmr": hmr}
+workflows = {"echo.hmr": hmr, "echo.upload": file_upload}
 
 
 def put(func: Callable):
